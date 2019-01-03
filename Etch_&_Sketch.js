292eed636a5167ea03d29f5324357de017529b3f
Brick <contact@lefside.com>
2019-01-02 21:39:31 -0800

    Seventh Commit

--- Etch_&_Sketch.js
+++ Etch_&_Sketch.js
@@ -3,34 +3,32 @@
 var id = 0;
 
 function createT() {
+
+//reference for table
+var tbl = document.getElementById("tbl");
+
+for(var r = 0; r< 5; r++)
+{
+    var row = document.createElement("tr");
+
+    for(var c = 0; c < 5; c++)
+    {
+       var cell = document.createElement("td");
+       cell.width = "20px;
+       cell.height = "20px"";
+       cell.id = id++;
+       
+       
+    }
+row.appendChild(cell);
+cell.innerHTML = id;
+
+}
+    tbl.appendChild(row);
+}
+ 
     
  
 
-    //get the reference for table
-    var tbl = document.getElementById("tbl");
-
-    //Here begins nested loops
-    for(var r = 0; r < 5; r++)
-    {
-
-var row = document.createElement("tr");
-
-
-for(var c = 0; c < 5; c++)
-{
-
-// Create the table cells
-var cell = document.createElement("td");
-HTMLTableCellElement.width = "20px";
-HTMLTableCellElement.height = "20px"; 
-cell.id = id++;
-
-
-}
-row.appendChild(cell);
-cell.innerHTML = id;
-    }
-tbl.appendChild(row);
-}
-
+   
 </script>
