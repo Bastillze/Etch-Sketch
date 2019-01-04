a2e190c3b417958d72219092d5f3f6bb9501937f
Brick <contact@lefside.com>
2019-01-04 00:32:29 -0800

    Second Commit

--- Etch_&_Sketch.js
+++ Etch_&_Sketch.js
@@ -3,6 +3,17 @@
 function myGrid() {
     
     const container = document.querySelector('#container');
+
+    for ( var i = 0; i < v; i++){
+
+       var row = document.createElement('div');
+       row .className = "row";
+       for(var x = 1; x <= v; x++){
+
+
+        
+       }
+    }
 }
  
 
