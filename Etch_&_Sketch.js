ae6f69defdbc8b80bfabb4379df9fcd5eade1c25
Brick <contact@lefside.com>
2019-01-04 00:10:57 -0800

    First Commit

--- Etch_&_Sketch.js
+++ Etch_&_Sketch.js
@@ -1,33 +1,9 @@
 <script>
 
-var id = 0;
-
-function createT() {
-
-//reference for table
-var tbl = document.getElementById("tbl");
-
-for(var r = 0; r< 15; r++)
-{
-    var row = document.createElement("tr");
-
-    for(var c = 0; c < 15; c++)
-    {
-       var cell = document.createElement("td");
-       cell.width = "20px";
-       cell.height = "20px";
-       cell.id = id++;
-       
-       
-    }
-row.appendChild(cell);
-cell.innerHTML = id;
-
-}
-    tbl.appendChild(row);
-}
- 
+function myGrid() {
     
+    const container = document.querySelector('#container');
+}
  
 
    
