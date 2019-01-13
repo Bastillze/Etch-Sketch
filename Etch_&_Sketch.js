7bc5cca7e944baa3f7b407f81ec091efadbc26a7
Brick <contact@lefside.com>
2019-01-13 02:22:59 -0500

    Updated

--- Etch_&_Sketch.js
+++ Etch_&_Sketch.js
@@ -1,12 +1,22 @@
-<script>
-
-function changeColor(evt) {
-    
-    var clickedOn = evt.target;
-
-    clickedOn.style.backgroundColor = '#f00';
-
+function generateGrid( rows, cols ) {
+    var grid = "<table>";
+    for ( row = 1; row <= rows; row++ ) {
+        grid += "<tr>"; 
+        for ( col = 1; col <= cols; col++ ) {      
+            grid += "<td></td>";
+        }
+        grid += "</tr>"; 
+    }
+    return grid;
 }
-  mySquare.addEventListener('click',changeColor,false); 
 
-</script>
+$( "#tableContainer" ).append( generateGrid( 5, 5) );
+
+$( "td" ).click(function() {
+    var index = $( "td" ).index( this );
+    var row = Math.floor( ( index ) / 5) + 1;
+    var col = ( index % 5 ) + 1;
+    $( "span" ).text( "That was row " + row + " and col " + col );
+    $( this ).css( 'background-color', 'red' );
+});
+
